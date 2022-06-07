34cfb1c1040b2e73e8ff5fb4f5b951a6df8cd777
Hersongomez88 <herson.gomez@mercadolibre.com.co>
2022-06-07 14:48:36 -0500

    API call and show response

--- app/src/main/java/com/example/notboredintegrador/DetailsActivity.kt
+++ app/src/main/java/com/example/notboredintegrador/DetailsActivity.kt
@@ -3,17 +3,67 @@ package com.example.notboredintegrador
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.notboredintegrador.databinding.ActivityDetailsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val activityType = intent.getStringExtra("ActivityType")
-        val participants = intent.getStringExtra("Participants")
+        val participants = intent.getStringExtra("Participants")?.toInt()
 
         println("Mensaje --> $activityType recibido y el numero de participantes es: $participants")
         val texto = findViewById<TextView>(R.id.TvActicityType)
         texto.setText("La actividad elegida es: ${activityType} \ny el numero de participantes para esta actividad es: $participants")
+
+        loadActivity(activityType, participants)
+        getRetroFit()
+
+
+    }
+
+    private fun loadActivity(activityType: String?, participants: Int?) {
+
+        CoroutineScope(Dispatchers.IO).launch {
+
+            val apiResponse = getRetroFit()
+                .create(APIService::class.java)
+                .getActivity(participants,activityType)
+
+            val activityInfo = apiResponse.body()
+
+            if (apiResponse.isSuccessful){
+
+                runOnUiThread{
+
+                    binding.TvActicity.text = activityInfo?.description ?: ""
+                    binding.TvActicityType.text = activityInfo?.category ?: ""
+                    binding.TvParticipants.text = activityInfo?.participants.toString() ?: "0"
+                    binding.TvPrice.text = activityInfo?.price.toString()
+                }
+
+
+
+            }else{
+
+            }
+
+        }
+
+    }
+
+    private fun getRetroFit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://www.boredapi.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
